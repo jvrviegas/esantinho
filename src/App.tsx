e05ce24c0c179776bf6d598598740c3d1a4dd011
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2020-11-27 21:25:25 -0300

    🎉 - Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { View, StatusBar } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <View />
+    </>
+  );
+};
+
+export default App;
