38dcc31434dc65a5c4452c713b8a0cf71dcc3a1e
João Vitor Viegas Rêgo <jvrviegas@gmail.com>
2020-11-28 02:41:09 -0300

    🎨 - SignIn page - Update styles - Add navigation to Dashboard

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
+import { View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import Button from '~/components/Button';
 import Input from '~/components/Input';
 
-import { Container, Content, Title, Illustration } from './styles';
+import {
+  Container,
+  Content,
+  Title,
+  LoginText,
+  Illustration,
+  CreateAccountButton,
+} from './styles';
 
 const SignIn: React.FC = () => {
   const { navigate } = useNavigation();
 
   return (
-    <Container>
-      <Content>
-        <Title>E-Santinho</Title>
-        <Illustration />
-        <Input name="email" placeholder="E-mail" />
-        <Input name="password" secureTextEntry placeholder="Senha" />
-        <Button onPress={() => navigate('SignUp')}>Cadastrar</Button>
-      </Content>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Content>
+              <Title>E-Santinho</Title>
+              <Illustration />
+              <View>
+                <LoginText>Faça seu logon</LoginText>
+              </View>
+              <Input name="email" placeholder="E-mail" />
+              <Input name="password" secureTextEntry placeholder="Senha" />
+              <Button onPress={() => navigate('Dashboard')}>Entrar</Button>
+            </Content>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton.Container onPress={() => navigate('SignUp')}>
+        <CreateAccountButton.Text>Criar uma conta</CreateAccountButton.Text>
+      </CreateAccountButton.Container>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 import { cw, ch } from '~/utils/calculateProportion';
 
 import Image from '~/assets/illustrations/image01.png';
@@ -10,12 +11,18 @@ export const Container = styled.SafeAreaView`
 export const Content = styled.View`
   flex: 1;
   align-items: center;
-  padding: ${ch(15)}px ${cw(30)}px;
+  padding: ${ch(15)}px ${cw(30)}px ${ch(10)}px;
 `;
 
 export const Title = styled.Text`
   font-family: 'OpenSans-Bold';
   font-size: ${cw(30)}px;
+  margin: ${ch(15)}px 0 0;
+`;
+
+export const LoginText = styled.Text`
+  font-family: 'OpenSans-Semibold';
+  font-size: ${cw(20)}px;
   margin: ${ch(15)}px 0;
 `;
 
@@ -23,5 +30,28 @@ export const Illustration = styled.Image.attrs({
   source: Image,
   resizeMode: 'contain',
 })`
-  height: 30%;
+  width: ${cw(300)}px;
+  height: ${ch(150)}px;
 `;
+
+export const CreateAccountButton = {
+  Container: styled.TouchableOpacity`
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: #fff;
+    border-top-width: 1px;
+    border-color: #ddd;
+    padding: ${ch(16)}px 0 ${ch(16 + getBottomSpace())}px;
+
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  `,
+  Text: styled.Text`
+    color: #333;
+    font-family: 'OpenSans-Regular';
+    font-size: ${cw(16)}px;
+  `,
+};
